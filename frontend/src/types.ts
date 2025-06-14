24187bffc023bd356b965fae5cec6a7ae2efecd1
Hyunwoo Kim <admin@hyunwoo.kim>
2025-06-15 00:31:19 +0900

    feat: add types

---
+++ frontend/src/types.ts
@@ -0,0 +1,32 @@
+import { ChatCompletionChunk } from 'openai/resources';
+
+export type Role = 'user' | 'assistant';
+
+export interface QueryRouting {
+  query_routing?: {
+    grades: Array<{
+      grade_label: string;
+      grade_value: number;
+      model: string;
+      score: number;
+    }>;
+    selected_model: string;
+  };
+}
+
+export interface BaseMessage {
+  id: string;
+  role: Role;
+  created: number;
+  content: string;
+}
+export interface UserMessage extends BaseMessage {
+  role: 'user';
+}
+export interface AssistantMessage extends BaseMessage, QueryRouting {
+  role: 'assistant';
+  model: string;
+}
+export type Message = UserMessage | AssistantMessage;
+
+export type FirstChunk = ChatCompletionChunk & QueryRouting;
