4060472ec4f0748d350cc3066b02ead032bc37a9
Hyunwoo Kim <admin@hyunwoo.kim>
2025-06-14 17:53:55 +0900

    feat: render message from stream

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,40 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
+import Markdown from 'react-markdown';
 import { useOpenAI } from './hooks/useOpenAI';
 
 export function App() {
   const openai = useOpenAI();
+  const [message, setMessage] = useState<string>('');
 
-  useEffect(() => {
-    (async () => {
-      const completion = await openai.chat.completions.create({
-        model: 'openai/gpt-4o-mini-2024-07-18',
-        messages: [
-          {
-            role: 'system',
-            content: 'You are a helpful assistant.',
-          },
-          {
-            role: 'user',
-            content: 'What are some highly rated restaurants in San Francisco?',
-          },
-        ],
-        stream: true,
-      });
+  async function handleStream() {
+    const completion = await openai.chat.completions.create({
+      model: 'openai/gpt-4o-mini-2024-07-18',
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a helpful assistant.',
+        },
+        {
+          role: 'user',
+          content: 'What are some highly rated restaurants in San Francisco?',
+        },
+      ],
+      stream: true,
+    });
 
-      for await (const chunk of completion) {
-        console.log(chunk.choices[0].delta.content);
+    for await (const chunk of completion) {
+      const delta = chunk.choices[0]?.delta?.content;
+      if (delta) {
+        setMessage(prev => prev + delta);
       }
-    })();
-  }, []);
+    }
+  }
 
-  return <></>;
+  return (
+    <>
+      <button onClick={handleStream}>Start</button>
+      <br />
+      <Markdown>{message}</Markdown>
+    </>
+  );
 }
