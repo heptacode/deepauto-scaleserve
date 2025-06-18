1241366fce22002d49c9e4f0bc2f26c631218380
Hyunwoo Kim <admin@hyunwoo.kim>
2025-06-19 02:20:38 +0900

    feat: define api remotes

--- frontend/src/remotes/chats.ts
+++ frontend/src/remotes/chats.ts
@@ -1,13 +1,38 @@
-import { Chat } from '../types';
+import { APIChat, Chat, ChatList, Message } from '../types';
 
 const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
 
-export async function getChats(): Promise<Chat[]> {
+export async function getChats(): Promise<ChatList[]> {
   const response = await fetch(`${BASE_URL}/chats`);
-  return (await response.json()) as Chat[];
+  return (await response.json()) as ChatList[];
 }
 
-export async function createChat({ title }: { title: string }): Promise<Chat> {
-  const response = await fetch(`${BASE_URL}/chats`, { method: 'POST', body: JSON.stringify({ title }) });
-  return (await response.json()) as Chat;
+export async function createChat(): Promise<Chat> {
+  const response = await fetch(`${BASE_URL}/chats`, { method: 'POST' });
+  const apiChat = (await response.json()) as APIChat;
+  return {
+    ...apiChat,
+    messages: new Map(apiChat.messages?.map(message => [message.id, message]) ?? []),
+  } satisfies Chat;
+}
+
+export async function getChat(chatId: string): Promise<Chat> {
+  const response = await fetch(`${BASE_URL}/chats/${chatId}`);
+  if (!response.ok) {
+    throw new Error('Chat not found');
+  }
+  const apiChat = (await response.json()) as APIChat;
+  return {
+    ...apiChat,
+    messages: new Map(apiChat.messages?.map(message => [message.id, message]) ?? []),
+  } satisfies Chat;
+}
+
+export async function createMessage(message: Message): Promise<Message> {
+  const response = await fetch(`${BASE_URL}/chats/${message.chatId}/messages`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ message }),
+  });
+  return (await response.json()) as Message;
 }
