9d1a0b75ff88e525fc67d5e01df5d0821855b32f
Hyunwoo Kim <admin@hyunwoo.kim>
2025-06-19 00:47:18 +0900

    feat: add chat list rendering

--- frontend/src/components/ChatListView.tsx
+++ frontend/src/components/ChatListView.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ComposeSVG from '../assets/icons/compose.svg';
+import { getChats } from '../remotes/chats';
+import { Chat } from '../types';
 
 export function ChatListView() {
+  const [chats, setChats] = useState<Chat[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getChats().then(setChats);
+  }, []);
+
   return (
     <aside>
       <div className="min-w-[200px] h-full px-3 py-4 overflow-y-auto bg-gray-50">
@@ -16,11 +27,20 @@ export function ChatListView() {
 
         <h6 className="text-sm text-gray-400">Chats</h6>
         <ul className="space-y-1 font-medium">
-          <li>
-            <a href="#" className="p-2 flex items-center rounded-lg text-gray-900 hover:bg-gray-100">
-              New Chat
-            </a>
-          </li>
+          {chats.map(chat => (
+            <li key={chat.id}>
+              <a
+                href={`/${chat.id}`}
+                className="p-2 flex items-center rounded-lg text-gray-900 hover:bg-gray-100"
+                onClick={e => {
+                  e.preventDefault();
+                  navigate(`/${chat.id}`);
+                }}
+              >
+                {chat.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </aside>
